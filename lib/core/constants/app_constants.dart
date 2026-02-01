964e87e30a9aff5f31b8f2ca9dd37af3616bd0ed
2135 <masoud.shayei@gmail.com>
2026-02-01 09:38:04 +0330

    Update API_BASE_URL in .env and refactor baseUrl in AppConstants to use the new remote server address

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,8 +2,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   // API Configuration
-  static String get baseUrl =>
-      dotenv.env['API_BASE_URL'] ?? 'http://localhost:3000';
+  // static String get baseUrl =>
+  // dotenv.env['API_BASE_URL'] ?? 'http://localhost:3000';
+  static const String baseUrl = 'http://10.10.40.235:3000';
   static const String apiVersion = '/api/v1';
 
   // Endpoints
